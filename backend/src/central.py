70c779c256b7df45e7d44cf38448de0fc63f315e
eduvasconcellos <e196240@dac.unicamp.br>
2021-10-11 18:00:45 -0300

    Começo do código para login

---
+++ backend/src/central.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class Central:
+    def __init__(self):
+        pass
+
+    def login(user, password):
+        '''
+        Método que realiza o login de um usuário no sistema. Retorna null em
+        caso de falha e um objeto do tipo usuário em caso de sucesso.
+        '''
+        '''
+        user : string com a identificação do usuário (ra)
+        password : string com a senha da DAC do usuário
+        '''
+
+        # Usa a api pra realizar autenticação
+        json = {'username': user, 'password': password}
+        response = requests.post(
+            "https://sigpos-api-prod.d2d.ic.unicamp.br/login", json=json)
+
+        # Se o login for um sucesso
+        if (response.status_code == 200):
+            pass
+        # Se o login falhar
+        else:
+            pass
