369eb4d5188286ccbfb16f0eb64b48aac1b1999e
Rebeca Stroh <rebecapstroh@gmail.com>
2021-10-18 22:22:04 -0300

    centralize and style

--- my-app/src/container/EditProfile.js
+++ my-app/src/container/EditProfile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from './Container';
-import { Icon, Checkbox, Input } from 'semantic-ui-react';
+import { Icon, Checkbox } from 'semantic-ui-react';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -11,23 +11,30 @@ export default class Login extends React.Component {
 
     deleteInfo = (info) => {
         let { information } = this.state;
-        information = information.filter(item => item != info );
-        this.setState({information})
+        information = information.filter(item => item !== info );
+        this.setState({information});
+    }
+
+    infoExist = (info) => {
+        let exists = false;
+        this.state.information.map(item => { if (item === info) exists = true; })
+        return exists;
     }
 
     addInfo = () => {
         if (!this.state.infoText) return null;
+        if (this.infoExist(this.state.infoText)) return null;
 
         const { information } = this.state;
-        information.push(this.state.infoText)
-        this.setState({information, infoText: ''})
+        information.push(this.state.infoText);
+        this.setState({information, infoText: ''});
     }
 
     renderInfo = (info, index) => {
         return(
             <div style={{borderRadius: 15, fontSize:11, backgroundColor: '#F1F1F1', paddingLeft: 10, paddingRight: 5, border: '1px solid #586973', marginLeft:10}}>
                 {info}
-                <button onClick={() => this.deleteInfo(info)} style={{marginLeft:5, color: '#586973', border: 'none'}}> x </button>
+                <button onClick={() => this.deleteInfo(info)} style={{marginLeft:5, color: '#586973', border: 'none', cursor:'pointer'}}> x </button>
             </div>
         )
     }
@@ -35,9 +42,9 @@ export default class Login extends React.Component {
     render() {
         return (
         <Container>
-            <p style={{textAlign:'left', fontSize:30 }}>Editar perfil</p>
-            <div style={{ margin: 50 }}>
-                <div style={{display: 'flex', flexDirection:'row', width: '100%', justifyContent: 'start', alignItems: 'center', margin:10}}>
+            <p style={{textAlign:'left', fontSize:30, paddingLeft: '8%' }}>Editar perfil</p>
+            <div style={{display: 'flex', flexDirection:'column', alignItems: 'center' }}>
+                <div style={{display: 'flex', flexDirection:'row', alignItems: 'center'}}>
                     <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center', width:100, height:100, marginRight: 10}}>
                         <Icon name='user' size='huge' style={{margin:10}}/>
                         <a> Mudar foto </a>
@@ -45,19 +52,16 @@ export default class Login extends React.Component {
                     <div>
                         <div style={{display: 'flex', flexDirection:'column', textAlign: 'left'}}>
                             Nome:
-                            <input type="text" name="name" style={{ borderRadius: 10, width:400, height:35, border: '1px solid #586973' }} />
-                            {/* <Input style={{ width:400, height:35 }} /> */}
+                            <input type="text" name="name" style={{ borderRadius: 10, width:500, height:35, border: '1px solid #586973' }} />
                         </div>
                         <div style={{display: 'flex', flexDirection:'row', marginTop: 10}}>
                             <div style={{display: 'flex', flexDirection:'column', textAlign: 'left'}}>
                                 Curso:
-                                <input type="text" name="name" style={{ borderRadius: 10, width:190, height:35, border: '1px solid #586973' }} />
-                                {/* <Input style={{ width:190, height:35 }} /> */}
+                                <input type="text" name="name" style={{ borderRadius: 10, width:240, height:35, border: '1px solid #586973' }} />
                             </div>
                             <div style={{display: 'flex', flexDirection:'column', textAlign: 'left', marginLeft: 20}}>
                                 Link do currículo Lattes:
-                                <input type="text" name="name" style={{ borderRadius: 10, width:190, height:35, border: '1px solid #586973' }} />
-                                {/* <Input style={{ width:190, height:35 }} /> */}
+                                <input type="text" name="name" style={{ borderRadius: 10, width:240, height:35, border: '1px solid #586973' }} />
                             </div>
                         </div>
                     </div>
@@ -66,11 +70,10 @@ export default class Login extends React.Component {
                     Informações para contato:
                     <div>
                         <input onChange={e => this.setState({infoText: e.target.value})} value={this.state.infoText} type="text" style={{ borderRadius: 10, width:300, height:35, border: '1px solid #586973' }} />
-                        <button onClick={this.addInfo} style={{ borderRadius: 10, width:35, height:35, backgroundColor: '#0070A8', color:'white', cursor:'pointer', border: '1px solid #FFFFFF', marginRight: 10 }}>
+                        <button onClick={this.addInfo} style={{ borderRadius: 10, width:35, height:35, backgroundColor: '#0070A8', color:'white', cursor:'pointer', border: '1px solid #FFFFFF' }}>
                             +
                         </button>
-                        {/* <Input label={{ basic: true, content: '+', color: 'blue' }} labelPosition='right' style={{ width:300, height:35, marginRight: 10 }} /> */}
-                        <Checkbox label='Deixar as informações de contato públicas' />
+                        <Checkbox checked label='Deixar as informações de contato públicas' style={{ fontSize: 12, marginLeft: 20 }} />
                     </div>
                     <div style={{display: 'flex', flexDirection:'row', marginTop: 10}}>
                         {this.state.information.map((item) => this.renderInfo(item))}
