810956143f55803c9cd48d4c71498122fc05985b
Rebeca Stroh <rebecapstroh@gmail.com>
2021-10-19 00:07:37 -0300

    add file selector

--- my-app/src/container/EditProfile.js
+++ my-app/src/container/EditProfile.js
@@ -6,7 +6,7 @@ export default class Login extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {information: [], infoText: ''};
+        this.state = {information: [], infoText: '', fileSelected: null};
     }
 
     deleteInfo = (info) => {
@@ -30,7 +30,11 @@ export default class Login extends React.Component {
         this.setState({information, infoText: ''});
     }
 
-    renderInfo = (info, index) => {
+    fileSelectedHandler = (e) => {
+        this.setState({fileSelected: URL.createObjectURL(e.target.files[0])})
+    }
+
+    renderInfo = (info) => {
         return(
             <div style={{borderRadius: 15, fontSize:11, backgroundColor: '#F1F1F1', paddingLeft: 10, paddingRight: 5, border: '1px solid #586973', marginLeft:10}}>
                 {info}
@@ -45,9 +49,12 @@ export default class Login extends React.Component {
             <p style={{textAlign:'left', fontSize:30, paddingLeft: '8%' }}>Editar perfil</p>
             <div style={{display: 'flex', flexDirection:'column', alignItems: 'center' }}>
                 <div style={{display: 'flex', flexDirection:'row', alignItems: 'center'}}>
-                    <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center', width:100, height:100, marginRight: 10}}>
-                        <Icon name='user' size='huge' style={{margin:10}}/>
-                        <a> Mudar foto </a>
+                    <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center', width:150, height:150, marginRight: 10}}>
+                        {this.state.fileSelected ? <img style={{width:100, height:100, borderRadius:5}} src={this.state.fileSelected}/> : <Icon name='user' size='huge' style={{margin:10}}/>}
+                        <input ref={fileInput => this.fileInput = fileInput} style={{ display:'none' }} type="file" onChange={this.fileSelectedHandler}/>
+                        <button onClick={() => this.fileInput.click()} style={{ background: 'white', border: 'none', color: '#0070A8', cursor:'pointer' }}> 
+                            {this.state.fileSelected ? 'Mudar foto' : 'Adicionar uma foto'}
+                        </button>
                     </div>
                     <div>
                         <div style={{display: 'flex', flexDirection:'column', textAlign: 'left'}}>
