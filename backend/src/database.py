7a0ee0734bdf83c42750f97f718ad24242515586
Victoria Pedrazzoli <v206664@dac.unicamp.br>
2021-11-10 00:00:23 -0300

    Filter searches complete

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,5 +1,6 @@
 from database_connection import DatabaseConnection
 from datetime import date
+from order_by import ORDER
 
 
 class Database:
@@ -8,7 +9,7 @@ class Database:
     def read_category(nome=None):
         '''
         ***Perform a read (SELECT) operation on the category table and returns the result.
-        *** Expects: none or the category name 
+        *** Expects: none or the category name
         *** Return:  id and name (optional)
         '''
         db_connection = DatabaseConnection()
@@ -31,7 +32,7 @@ class Database:
     def read_tag(id_tag=None, palavra_chave=None):
         '''
         ***Perform a read (SELECT) operation on the tag table and returns the result.
-        *** Expects: none or the id or the tag name 
+        *** Expects: none or the id or the tag name
         *** Return:  id and name (optional)
         '''
         db_connection = DatabaseConnection()
@@ -57,7 +58,7 @@ class Database:
     def read_user(args):
         '''
         ***Perform a read (SELECT) operation on the user table and returns the result.
-        *** Expects: an email and password 
+        *** Expects: an email and password
         *** Return: the user fields
         '''
         db_connection = DatabaseConnection()
@@ -116,7 +117,7 @@ class Database:
             db_connection.close_all()
 
     @staticmethod
-    def insert_pesquisa(titulo, descricao, idCategoria, ano_inicio, git=None, tag_1=None, tag_2=None, tag_3=None):
+    def insert_search(titulo, descricao, idCategoria, ano_inicio, git=None, tag_1=None, tag_2=None, tag_3=None):
         '''
         *** Performs a write (INSERT) operation on the Search Table.
         *** Expects: an title, description, idCategory, start year, git, tag_1, tag_2, tag_3
@@ -192,6 +193,107 @@ class Database:
             db_connection.connection.commit()
         except Exception as e:
             db_connection.connection.rollback()
+            raise(e)
+        finally:
+            db_connection.close_all()
+
+    @staticmethod
+    def insert_attachments(caminho, idPesquisa=None, idPublicacao=None):
+        '''
+        *** Performs a write (INSERT) operation on the Attachments Table.
+        *** Expects: an path, and a Serch or Publication
+        '''
+        try:
+            db_connection = DatabaseConnection()
+            cursor = db_connection.connection.cursor()
+
+            query = "INSERT INTO `Anexos` (`caminho`"
+            if(idPesquisa is None) and (idPublicacao is None):
+                raise(Exception("Missing arguments"))
+            if(not idPesquisa is None):
+                query += ", `idPesquisa`"
+            if(not idPublicacao is None):
+                query += ", `idPublicacao`"
+            query += ") VALUES ('" + caminho
+            if(not idPesquisa is None):
+                query += "', '" + idPesquisa
+            if(not idPublicacao is None):
+                query += "', '" + idPublicacao
+            query += "');"
+            print(query)
+            cursor.execute(query)
+            db_connection.connection.commit()
+        except Exception as e:
+            db_connection.connection.rollback()
+            raise(e)
+        finally:
+            db_connection.close_all()
+
+    @staticmethod
+    def read_searches(ord=None, titulo=None, idCategoria=None, idAutor=None, idTag=None):
+        '''
+        *** Performs a search (SELECT) operation on the Search Table.
+        *** Expects: ord: one of the enums values
+                     all other fields are optionals, none null filds should be lists
+        *** Return: all fields
+        '''
+        try:
+            db_connection = DatabaseConnection()
+            cursor = db_connection.connection.cursor()
+            first = 0
+
+            query = "SELECT Pesquisas.idPesquisa, Pesquisas.titulo, Pesquisas.descricao, Pesquisas.idCategoria, "
+            query += "Pesquisas.ano_inicio, Pesquisas.estimativa_fim, Pesquisas.numero_likes, Pesquisas.idTag_1, Pesquisas.idTag_2, "
+            query += "Pesquisas.idTag_3, Pesquisas.ultima_atualizacao, Pesquisas.git FROM Pesquisas\n"
+            if(titulo is None) and (idTag is None) and (idCategoria is None) and (idAutor is None):
+                query += "INNER JOIN Autores ON Pesquisas.idPesquisa = Autores.idPesquisa"
+            else:
+                query += "INNER JOIN Autores ON Pesquisas.idPesquisa = Autores.idPesquisa WHERE ("
+                if(not titulo is None):
+                    first = 1
+                    query += "\n\t(Pesquisas.titulo LIKE '%" + titulo[0] + "%'"
+                    for _t in titulo[1:]:
+                        query += "\n\t OR Pesquisas.titulo LIKE '%" + _t + "%'"
+                    query += ")\n "
+                if(not idAutor is None):
+                    if(first == 1):
+                        query += "\tAND "
+                    first = 1
+                    query += "\n\t(Autores.idAutores = '" + idAutor[0] + "'"
+                    for _a in idAutor[1:]:
+                        query += "\n\t OR Autores.idAutores = '" + _a + "'"
+                    query += ")\n"
+                if(not idCategoria is None):
+                    if(first == 1):
+                        query += "\tAND "
+                    first = 1
+                    query += "\n\t(Pesquisas.idCategoria is NULL "
+                    for _c in idCategoria:
+                        query += "\n\t OR Pesquisas.idCategoria = '" + _c + "'"
+                    query += ")\n"
+                if(not idTag is None):
+                    if(first == 1):
+                        query += "\tAND "
+                    query += "\n\t(Pesquisas.idTag_1 is NULL "
+                    query += "\n\t OR Pesquisas.idTag_2 is NULL "
+                    query += "\n\t OR Pesquisas.idTag_3 is NULL "
+                    for _t in idTag:
+                        query += "\n\t OR Pesquisas.idTag_1 = '" + _t + "'"
+                        query += "\n\t OR Pesquisas.idTag_2 = '" + _t + "'"
+                        query += "\n\t OR Pesquisas.idTag_3 = '" + _t + "'"
+                    query += ")\n"
+                query += ")"
+            if (ord is None) or (ORDER(ord).value == ORDER.autor.value):
+                query += " ORDER BY Autores.nome ASC,\n"
+            else:
+                query += " ORDER BY Pesquisas." + ORDER(ord).name + " ASC,\n"
+            query += "\t case when idCategoria is null then 1 else 0 end, idCategoria,\n"
+            query += "\t case when idTag_1 is null then 1 else 0 end, idTag_1,\n"
+            query += "\t case when idTag_2 is null then 1 else 0 end, idTag_2,\n"
+            query += "\t case when idTag_3 is null then 1 else 0 end, idTag_3;\n"
+            cursor.execute(query)
+            return cursor.fetchall()
+        except Exception as e:
             raise(e)
         finally:
             db_connection.close_all()
