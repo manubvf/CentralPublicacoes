7028e28fdf1097d82e16af20a0b0055d883832df
Manoela <manoela.bvf@gmail.com>
2021-11-14 20:29:36 -0300

    Creating notifications component

---
+++ my-app/src/components/NotificationItem.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import star from '../images/starNotification.png';
+import addFile from '../images/addFileNotification.png';
+import addProject from '../images/addProjectNotification.png';
+import { Icon } from 'semantic-ui-react';
+import {
+  Link,
+} from 'react-router-dom'
+
+const styles = {
+    itemContainer: {
+        display: 'flex',
+        borderBottom: '1px solid #B5BBBF',
+        width: '100%',
+        marginBottom: 14,
+    },
+    icon:{
+      width: 29,
+      height: 29,
+      objectFit: 'contain',
+    },
+    title: {
+      fontSize: 17,
+      fontWeight: 'bold',
+      color: '#333D42',
+      marginLeft: 15,
+      marginTop: 3,
+    },
+    link: {
+      color: "#0070A8",
+      textDecoration: 'underline',
+      fontSize: 17,
+      fontWeight: 'bold',
+      marginTop: 5,
+      marginLeft: 3,
+    },
+    subtitle:{
+      fontSize: 15,
+      color: '#586973',
+      marginTop: 4,
+      marginLeft: 6,
+      marginRight: 7,
+    },
+    date:{
+      fontSize: 15,
+      color: '#586973',
+      marginTop: 4,
+      position: 'absolute',
+      right: '7%',
+    },
+    button:{
+      cursor: 'pointer',
+      color: '#0070A8',
+      fontSize: 26,
+      marginLeft: 3,
+      marginTop: 2,
+    },
+  };
+
+  export default class NotificationItem extends React.Component {
+
+    constructor(props) {
+      super(props);
+
+      let iconImg = this.props.type === "like" ? star : addFile;
+      if (this.props.type === "newProject"){
+        iconImg = addProject;
+      }
+
+      let titleText = this.props.type === "like" ? " demonstrou interesse em seu projeto " : " adicionou um novo documento ao projeto ";
+      if (this.props.type === "newProject"){
+        titleText = " te definiu como um dos autores do projeto ";
+      }
+
+      let subtitleText = this.props.type === "newProject" ? " - Você é autor desse projeto? " : " - Você concorda com a mudança? ";
+
+      this.state = {icon: iconImg, text: titleText, subtitle: subtitleText};
+    }
+
+    render() {
+      return (
+          <div style={styles.itemContainer}>
+            <img style={styles.icon} src={this.state.icon}/>
+            <p style={styles.title}>{this.props.userName}{this.state.text}</p>
+            <Link style={styles.link} to="/">“{this.props.projectTitle}”</Link>
+            { this.props.needsConfirmation === true ? <p style={styles.subtitle}>{this.state.subtitle}</p>:null}
+            { this.props.needsConfirmation === true ? <Icon style={styles.button} name="check circle" size='large' id="ok" onClick={this.props.confirmationEvent}/>:null}
+            { this.props.needsConfirmation === true ? <Icon style={styles.button} name="times circle outline" size='large' id="no" onClick={this.props.confirmationEvent}/>:null}
+            <p style={styles.date}>{this.props.date}</p>
+          </div>
+      );
+    }
+  }
+
+
+  /*************************************************
+  NotificationItem Component Documentation:
+
+  - Props:
+    - type: "like", "newFile", "newProject"
+    - userName: user who "sent" the notification
+    - projectTitle
+    - projectLink
+    - date
+
+  - Optional Props:
+    - needsConfirmation
+    - confirmationEvent: icon onClick event
+
+  ************************************************/
--- my-app/src/container/Notifications.js
+++ my-app/src/container/Notifications.js
@@ -1,12 +1,27 @@
 
 import React from 'react';
 import Container from './Container';
+import NotificationItem from '../components/NotificationItem';
 
 export default class Notifications extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleConfirmation = this.handleConfirmation.bind(this);
+    }
+
+    handleConfirmation(event){
+      console.log("EVENTO ", event.target.id);
+    }
+
     render() {
       return (
         <Container>
-            <p style={{ fontSize:30 }}>Notifications</p>
+          <p style={{ fontSize:30, paddingLeft: '8%' }}>Notificações</p>
+          <div style={{ display: 'flex', flexDirection: 'column'}}>
+            <NotificationItem type="like" userName="Hilda" projectTitle="Título do projeto" date="15 Fev 2021"/>
+            <NotificationItem type="newFile" userName="Frida" projectTitle="Título da pesquisa" date="12 Fev 2021" needsConfirmation={true} confirmationEvent={this.handleConfirmation}/>
+            <NotificationItem type="newProject" userName="David"  projectTitle="Título de qualquer coisa" date="3 Fev 2021" needsConfirmation={true} confirmationEvent={this.handleConfirmation}/>
+          </div>
         </Container>
       );
     }
